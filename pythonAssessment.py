2e86aa618ef61e104f69318c8369c2613fc32629
Gerry <gerrywiegand@gmail.com>
2025-10-26 08:39:10 -0400

    defined functions(blank)

---
+++ pythonAssessment.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def count_specific_word(str,str):
+
+
+def identify_most_common_word(str):
+
+def calculate_average_word_length(str):
+
+def count_paragraphs(str):
+
+def count_sentences(str):
